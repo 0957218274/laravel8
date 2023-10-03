ca08ea666ae5794d0f11ea10c8b0af738daea886
Beach123za <137165847+Beach123za@users.noreply.github.com>
2023-10-03 14:58:56 +0700

    tes4.4

--- resources/views/myprofile/create.blade.php
+++ resources/views/myprofile/create.blade.php
@@ -4,26 +4,11 @@
 <form action="#" method="POST">
     <div>
         <strong>Name : </strong>
-        <input type="text" />
+        <input type="text" value="{{ request('name') }}" />
     </div>
     <div>
         <strong>Lastname : </strong>
-        <input type=<h1>Create My Profile</h1>
-        <form action="#" method="POST">
-            <div>
-                <strong>Name : </strong>
-                <input type="text" value="{{ request('name') }}" />
-            </div>
-            <div>
-                <strong>Lastname : </strong>
-                <input type="text" value="{{ request('lastname') }}" />
-            </div>
-            ...
-        </form>"text" />
+        <input type="text" value="{{ request('lastname') }}" />
     </div>
-    <div>
-        <strong>Email : </strong>
-        <input type="text" />
-    </div>
-    <div><button type="submit">Add</button></div>
-</form>
+</form> 
+
